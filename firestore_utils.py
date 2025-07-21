b48f5bbb56753e536595bce61d1b2759ba061780
rmoharana038 <85046291+rmoharana038@users.noreply.github.com>
2025-07-21 09:35:44 +0530

    Update firestore_utils.py

--- firestore_utils.py
+++ firestore_utils.py
@@ -30,7 +30,7 @@ def init_firestore():
                 "auth_uri": "https://accounts.google.com/o/oauth2/auth",
                 "token_uri": "https://oauth2.googleapis.com/token",
                 "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-                "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL")
+                "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
             })
             firebase_admin.initialize_app(cred, {
                 'projectId': os.getenv("FIREBASE_PROJECT_ID")
